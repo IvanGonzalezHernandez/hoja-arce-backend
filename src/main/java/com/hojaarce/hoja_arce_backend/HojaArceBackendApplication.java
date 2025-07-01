8a8422cf64653e6fd0087ad203bddb2b83188b98
IvanGonzalezHernandez <ivangonzalez.code@gmail.com>
2025-07-01 20:30:25 +0200

    Subiendo proyecto

---
+++ src/main/java/com/hojaarce/hoja_arce_backend/HojaArceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hojaarce.hoja_arce_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HojaArceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HojaArceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hojaarce/hoja_arce_backend/HojaArceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hojaarce.hoja_arce_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HojaArceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
